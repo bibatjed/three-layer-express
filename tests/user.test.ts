0028bf1d04ed641131bb0c02efca336bc962ff07
bibatjed <bibatjed.dev@gmail.com>
2024-01-24 18:57:26 +0800

    add test for server side and positive path user registration

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -3,24 +3,51 @@ import { IRepository } from "../src/repository";
 import UserService from "../src/services/user/User";
 import { IUserService } from "../src/controllers/User";
 import { ZodError } from "zod";
+import ErrorService from "../src/services/ErrorService";
 
 describe("User Service", () => {
   describe("User Registration", () => {
-    describe("User Registration Validation", () => {
-      let userService: IUserService;
-      beforeAll(() => {
-        const repository: IRepository = {
-          startTransaction: (callback) => {
-            return Promise.resolve(callback({} as Transaction));
-          },
-          user: {
-            findUserByEmail: jest.fn(),
-            registerUser: jest.fn(),
-            findUserById: jest.fn(),
-          },
-        };
-        userService = new UserService(repository);
+    let userService: IUserService;
+    let repository: IRepository;
+    beforeAll(() => {
+      repository = {
+        startTransaction: (callback) => {
+          return Promise.resolve(callback({} as Transaction));
+        },
+        user: {
+          findUserByEmail: jest.fn(),
+          registerUser: jest.fn(),
+          findUserById: jest.fn(),
+        },
+      };
+      userService = new UserService(repository);
+    });
+
+    describe("User Registration Positive Path", () => {
+      it("should return success message after user registration", async () => {
+        const result = await userService.register({ email: "registered@gmail.com", password: "12345678", firstName: "hello", lastName: "hello", middleName: "" });
+        expect(result).toMatchObject({ message: "User is registered" });
       });
+    });
+
+    describe("User Registration Server Validation", () => {
+      it("should throw an error if user is already registered", async () => {
+        try {
+          //@ts-expect-error
+          repository.user.findUserByEmail.mockResolvedValueOnce({ id: "1" });
+          await userService.register({ email: "registered@gmail.com", password: "12345678", firstName: "hello", lastName: "hello", middleName: "" });
+        } catch (e) {
+          if (e instanceof ErrorService) {
+            expect(e).toMatchObject({
+              status: 422,
+              message: "User is already registered",
+            });
+          }
+        }
+      });
+    });
+
+    describe("User Registration Client Validation", () => {
       it("should throw an error if user pass an empty object or none", async () => {
         try {
           //@ts-expect-error
